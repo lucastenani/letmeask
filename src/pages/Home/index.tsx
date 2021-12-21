aa244544f97cd19025b1a5d82ddb265153bf97f6
lucastenani <lucas.tenani3@gmail.com>
2021-12-21 15:20:20 -0300

    Create new room page created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import illustrationImg from "../../assets/images/illustration.svg";
 import logoImg from "../../assets/images/logo.svg";
 import googleIconImg from "../../assets/images/google-icon.svg";
-import "./style.scss";
+import "../../styles/auth.scss";
 import { Button } from "../../components/Button";
 
 export function Home() {
