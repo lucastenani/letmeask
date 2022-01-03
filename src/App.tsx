c94b06df766d63715c21a405d50932f69fa0cd57
lucastenani <lucas.tenani3@gmail.com>
2022-01-03 12:00:55 -0300

    Implementing the react router dom for the routes between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { NewRoom } from "./pages/NewRoom";
+import MainRoutes from "./routes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <NewRoom />;
+  return (
+    <Router>
+      <MainRoutes />
+    </Router>
+  );
 }
 
 export default App;
