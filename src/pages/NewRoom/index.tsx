2e8baeb2c552b06203ccf37d25af5a2a0798962e
lucastenani <lucas.tenani3@gmail.com>
2022-01-04 15:18:36 -0300

    Links applied to go from one page to another

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import illustrationImg from "../../assets/images/illustration.svg";
 import logoImg from "../../assets/images/logo.svg";
 import "../../styles/auth.scss";
@@ -23,7 +24,7 @@ export function NewRoom() {
             <Button type="submit">Create room</Button>
           </form>
           <p>
-            Want to join an existing room? <a href="./">click here</a>
+            Want to join an existing room? <Link to="/">click here</Link>
           </p>
         </div>
       </main>
