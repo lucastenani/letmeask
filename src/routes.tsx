9f678f63eeebb43556b8ef1ff77c1edd90ada8bd
lucastenani <lucas.tenani3@gmail.com>
2022-01-17 11:45:49 -0300

    Route to specific room has been created

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/rooms/new" element={<NewRoom />} />
+      <Route path="/rooms/:id" element={<Room />} />
     </Routes>
   );
 }
