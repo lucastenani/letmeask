378e84a62553c127d56f1ee17c5237dba8b429f9
lucastenani <lucas.tenani3@gmail.com>
2022-01-17 09:40:22 -0300

    Create a useEffect for the application to search Firebase if there is a logged in user recovering the authentication state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { auth, firebase } from "./services/firebase";
 import MainRoutes from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -18,6 +18,24 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google Account.");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }, []);
 
   async function sigInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
