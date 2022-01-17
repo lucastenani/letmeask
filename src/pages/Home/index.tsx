2adf6ae536cf5d7c89b9344ed75f388de9b5ac56
lucastenani <lucas.tenani3@gmail.com>
2022-01-17 12:08:07 -0300

    The entire flow used to enter a room that has already been created has been implemented

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,10 +5,13 @@ import googleIconImg from "../../assets/images/google-icon.svg";
 import "../../styles/auth.scss";
 import { Button } from "../../components/Button";
 import { useAuth } from "../../hooks/useAuth";
+import { FormEvent, useState } from "react";
+import { database } from "../../services/firebase";
 
 export function Home() {
   const navigate = useNavigate();
   const { user, sigInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function navigateToNewRoom() {
     if (!user) {
@@ -17,6 +20,24 @@ export function Home() {
 
     navigate("/rooms/new");
   }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event?.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("This room does not exist.");
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -35,8 +56,13 @@ export function Home() {
             Create your room using Google
           </button>
           <div id="separator">or enter a room</div>
-          <form>
-            <input type="text" placeholder="Enter room code" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Enter room code"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">Enter room</Button>
           </form>
         </div>
